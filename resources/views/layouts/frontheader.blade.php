f63e7f62981e79e57c17d33224ea2d39c2b302ae
Ahemad <webdeveloper14.intelliworkz@gmail.com>
2026-07-27 12:12:03 +0530

    changed favicon type and added sizes any

--- resources/views/layouts/frontheader.blade.php
+++ resources/views/layouts/frontheader.blade.php
@@ -33,7 +33,8 @@
     </script>
     @endif
 
-    <link rel="icon" href="{{ asset('public/front/assets/images/favicon.png') }}" type="image/x-icon">
+    <!-- <link rel="icon" href="{{ asset('public/front/assets/images/favicon.png') }}" type="image/x-icon"> -->
+    <link rel="icon" href="{{ asset('public/front/assets/images/favicon.png') }}" type="image/png" sizes="any">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
